60c6d66ed37b5ec618c3f0e0c6d5c224abcf5524
amolhisekar2002 <amolhisekar2002@gmail.com>
2026-02-14 23:21:26 +0530

    Power java program

---
+++ Power.java
@@ -0,0 +1,17 @@
+class Power{
+    public static void main(String[] args) {
+        int base = 2;
+        int rev = 5;
+        int res = 1;
+       /* while(rev >0){
+            res = res*base;
+            rev--;
+        }
+        System.out.println("2^5="+res);
+        */
+       for(int i=1; i<=rev;i++){
+        res = res * base;
+        System.err.println("2^5="+res);
+       }
+    }
+}
