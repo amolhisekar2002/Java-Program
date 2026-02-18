5befa0f8c76688a3734f88dc610ccc4fd8d8dcb1
amolhisekar2002 <amolhisekar2002@gmail.com>
2026-02-18 11:32:05 +0530

    NumberToWords.java program

---
+++ NumberToWords.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+class NumberToWords 
+{
+	public static void main(String[] args) 
+	{
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Entert the number :");
+		int num = sc.nextInt();
+
+		String word = " ";
+		for (int i = num; i!=0; i=i/10)
+		{
+			int ld = i%10;
+			switch (ld)
+			{
+			case 0 : word = "zero "+word; break;
+			case 1 : word = "one "+word; break;
+			case 2 : word = "two "+word; break;
+			case 3 : word = "three "+word; break;
+			case 4 : word = "four "+word; break;
+			case 5 : word = "five "+word; break;
+			case 6 : word = "six "+word; break;
+			case 7 : word = "seven "+word; break;
+			case 8 : word = "eight "+word; break;
+			case 9 : word = "nine "+word; break;
+
+			
+			}
+		}
+		System.out.println(word);
+	}
+}
+
