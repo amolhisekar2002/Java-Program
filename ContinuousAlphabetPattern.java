74933ce26511bb84bc210939c9e6a613652e19fd
amolhisekar2002 <amolhisekar2002@gmail.com>
2026-03-09 21:10:40 +0530

    ContinuousAlphabetPattern java program

---
+++ ContinuousAlphabetPattern.java
@@ -0,0 +1,16 @@
+class ContinuousAlphabetPattern{
+    public static void main(String[] args) {
+        
+        int n=4;
+        char ch='a';
+
+        for(int i=0; i<n; i++){
+            for (int j=0; j<n; j++) {
+                System.out.print(ch+" ");
+                ch++;
+            }
+            System.out.println();
+        }
+        
+    }
+}
