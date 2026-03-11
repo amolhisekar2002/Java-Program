daf55f08eba50369e8f70cdecbcb24503b684a2c
amolhisekar2002 <amolhisekar2002@gmail.com>
2026-03-11 23:21:43 +0530

    Number Pattern5 java program

---
+++ NumberPattern5.java
@@ -0,0 +1,16 @@
+public class NumberPattern5 {
+    public static void main(String[] args) {
+        
+        int n =4;
+        int num=1;
+
+        for(int i=0; i<n; i++){
+            for(int j=0; j<n; j++){
+                System.out.print(num+ " ");
+                num++;
+
+            }
+            System.out.println();
+        }
+    }
+}
