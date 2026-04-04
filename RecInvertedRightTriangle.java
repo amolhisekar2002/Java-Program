c908c283369a7d9aae736461287a9e7dbffffffb
amolhisekar2002 <amolhisekar2002@gmail.com>
2026-04-04 11:38:19 +0530

    RecInvertedRightTriangle java program

---
+++ RecInvertedRightTriangle.java
@@ -0,0 +1,19 @@
+class RecInvertedRightTriangle {
+    public static void main(String[] args) {
+        triangle(0,0,5);
+    }
+    public static void triangle(int i, int j, int n){
+        if(i>n){
+            return;
+        }
+        if(j<=n-i){
+            System.out.print("*"+" ");
+            triangle(i,j+1,n);
+        }
+        else{
+            System.out.println();
+            triangle(i+1,0, n);
+        }
+
+    }
+}
